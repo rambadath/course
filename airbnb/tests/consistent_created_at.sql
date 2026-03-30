e1a5b1998d2c0d7111ff1fc0a90c7529babf4903
Ram A <ram.it.pro@outlook.com>
2026-03-30 10:02:41 +0530

    commit consistent_created_at.sql

--- airbnb/tests/consistent_created_at.sql
+++ airbnb/tests/consistent_created_at.sql
@@ -1,4 +1,4 @@
-SELECT * FROM {{ ref('dim_listings_cleansed') }} l
+SELECT * FROM {{ ref('dim_list_cleansed') }} l
 INNER JOIN {{ ref('fct_reviews') }} r
 USING (listing_id)
 WHERE l.created_at > r.review_date
