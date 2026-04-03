721be14f0aa5619b5ce0a8d31fdd2ce3a7c43e44
Ram A <ram.it.pro@outlook.com>
2026-04-03 16:07:25 +0530

    commit

--- airbnb/macros/no_empty_strings.sql
+++ airbnb/macros/no_empty_strings.sql
@@ -1,8 +1,8 @@
-{% macro no_empty_strings(model) %}
+{% test no_empty_strings(model) %}
     {% for col in adapter.get_columns_in_relation(model) %}
         {% if col.is_string() %}
-            {{ col.name }} IS NOT NULL AND {{ col.name }} <> '' AND
+            {{ col.name }} is null or {{ col.name }} <> '' AND
         {% endif %}
     {% endfor %}
-    TRUE
-{% endmacro %}
+    FALSE
+{% endtest %}
