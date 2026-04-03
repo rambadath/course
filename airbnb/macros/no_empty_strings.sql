bf4538332352016e7e1d2d1e2bc9e7ac41e487f7
Ram A <ram.it.pro@outlook.com>
2026-04-03 15:18:39 +0530

    commit

--- airbnb/macros/no_empty_strings.sql
+++ airbnb/macros/no_empty_strings.sql
@@ -1,5 +1,3 @@
-{# Here is my solution after removing most of the white spaces #}
-
 {% macro no_empty_strings(model) %}
     {% for col in adapter.get_columns_in_relation(model) %}
         {% if col.is_string() %}
