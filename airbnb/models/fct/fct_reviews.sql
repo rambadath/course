33cedb1297bd07629eec3e511b3ad9cb256954b2
Ram A <ram.it.pro@outlook.com>
2026-04-07 11:55:13 +0530

    commit

--- airbnb/models/fct/fct_reviews.sql
+++ airbnb/models/fct/fct_reviews.sql
@@ -1,26 +1,34 @@
-{{
-  config(
+{{ config(
     materialized = 'incremental',
-    on_schema_change='fail'
-    )
-}}
+    on_schema_change = 'fail',
+    unique_key = 'review_id'
+) }}
+
 WITH src_reviews AS (
-  SELECT LISTING_ID,
-	REVIEW_DATE,
-	REVIEWER_NAME,
-	REVIEW_TEXT,
-	REVIEW_SENTIMENT FROM {{ ref('src_reviews') }}
+    SELECT
+        listing_id,
+        review_date,
+        reviewer_name,
+        review_text,
+        review_sentiment
+    FROM {{ ref('src_reviews') }}
 )
+
 SELECT
-  {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }}
-    AS review_id,
-  listing_id,
-  review_date,
-  reviewer_name,
-  review_text,
-  review_sentiment
-WHERE review_text is not null
+    {{ dbt_utils.generate_surrogate_key([
+        'listing_id',
+        'review_date',
+        'reviewer_name',
+        'review_text'
+    ]) }} AS review_id,
+    listing_id,
+    review_date,
+    reviewer_name,
+    review_text,
+    review_sentiment
+FROM src_reviews
+WHERE review_text IS NOT NULL
 
 {% if is_incremental() %}
-  AND review_date > (select max(review_date) from {{ this }})
+  AND review_date > (SELECT max(review_date) FROM {{ this }})
 {% endif %}
