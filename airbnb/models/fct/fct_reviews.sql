bf0df6aa0caf053409674dd45019224e767796f5
Ram A <ram.it.pro@outlook.com>
2026-04-07 11:39:13 +0530

    commit

--- airbnb/models/fct/fct_reviews.sql
+++ airbnb/models/fct/fct_reviews.sql
@@ -10,7 +10,11 @@ WITH src_reviews AS (
 SELECT
   {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }}
     AS review_id,
-  *
+  listing_id,
+  review_date,
+  reviewer_name,
+  review_text,
+  review_sentiment
 WHERE review_text is not null
 
 {% if is_incremental() %}
